528369a67519989732498f298954a042fd57adc7
Gopinath P <gopinath.p@tandemloop.in>
2025-02-16 09:34:10 +0530

    migrated from ng11 to ng18

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,35 +1,30 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { HostListener } from "@angular/core";
-import { SwiperComponent } from 'swiper/angular';
-import SwiperCore,{ Keyboard, Mousewheel, Navigation, Pagination } from 'swiper/core';
-SwiperCore.use([Navigation,Pagination,Mousewheel,Keyboard])
+import { Component, HostListener } from '@angular/core';
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrl: './app.component.css',
 })
-export class AppComponent implements OnInit, OnDestroy{
+export class AppComponent {
   title = 'smartfolio';
   arrow = true;
-  scrWidth:any;
+  scrWidth: any;
 
   @HostListener('window:resize', ['$event'])
   getScreenSize(event?: undefined) {
-    this.scrWidth = window.innerWidth;
-    if(this.scrWidth <= 600){
-      this.arrow = false;
-    }
-    else{
-      this.arrow = true;
+    if (typeof window !== 'undefined') {
+      // browser code
+      this.scrWidth = window.innerWidth;
+      if (this.scrWidth <= 600) {
+        this.arrow = false;
+      } else {
+        this.arrow = true;
+      }
     }
   }
-
 
   constructor() {
     this.getScreenSize();
   }
-
-
-  ngOnInit(){}
-  ngOnDestroy(){}
 }
