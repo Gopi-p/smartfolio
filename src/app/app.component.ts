cb3ae3a99be42783b5929dde30ece74ea87e379e
Gopinath P <gopinath.p@tandemloop.in>
2025-02-16 09:45:25 +0530

    Old dependencies removed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostListener } from '@angular/core';
+import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -6,25 +6,4 @@ import { Component, HostListener } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {
-  title = 'smartfolio';
-  arrow = true;
-  scrWidth: any;
-
-  @HostListener('window:resize', ['$event'])
-  getScreenSize(event?: undefined) {
-    if (typeof window !== 'undefined') {
-      // browser code
-      this.scrWidth = window.innerWidth;
-      if (this.scrWidth <= 600) {
-        this.arrow = false;
-      } else {
-        this.arrow = true;
-      }
-    }
-  }
-
-  constructor() {
-    this.getScreenSize();
-  }
-}
+export class AppComponent {}
